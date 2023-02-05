7b47cae161115c03977355999c0a8fe2a257d676
Mahafuj Ahamed <114424008+Mahafuj-Ahamed@users.noreply.github.com>
2023-02-05 10:14:14 -0800

    Add files via upload

---
+++ search.js
@@ -0,0 +1,15 @@
+const lyrics = 'Tahsan joy obaid';
+// console.log(lyrics);
+
+const result = Math.pow(3, 2);
+// console.log(result);
+
+// --------------------------------
+
+const number = 4.27867;
+const realNumber = Math.round(number);
+
+
+// console.log(realNumber) 
+
+
