f8f834c49c12648e42dd7781e02a8717220ff5cf
Mahafuj Ahamed <114424008+Mahafuj-Ahamed@users.noreply.github.com>
2023-02-05 10:13:19 -0800

    Add files via upload

---
+++ case.js
@@ -0,0 +1,10 @@
+const userName = 'blackPink';
+const userInput = 'blackpink';
+
+if (userName.toLocaleLowerCase() === userInput.toLocaleLowerCase()){
+    console.log('valid user');
+}
+else{
+    console.log('invalid user');
+}
+
